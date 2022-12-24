9d5f0cbec506127eda5d6ab0d9180871819dbc84
Brandon_Hart <sugb73@gmail.com>
2022-12-24 09:29:14 -0500

    Starting the project over due to api issues

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 import './App.css';
-import HomeScreen from './components/HomeScreen';
 
 function App() {
-  return (
-    <div className='App'>
-      <HomeScreen />
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
